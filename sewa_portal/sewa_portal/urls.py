faa8e2d0b390bb2e35852f2530b4bb23a7a91492
RohanMagar7 <magarohan8@gmail.com>
2025-10-15 17:48:09 +0530

    remove health py file in

--- sewa_portal/sewa_portal/urls.py
+++ sewa_portal/sewa_portal/urls.py
@@ -4,12 +4,12 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
-from core.health import health_check
+# from core.health import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Health check for Render keep-alive
-    path('health/', health_check, name='health_check'),
+    # path('health/', health_check, name='health_check'),
     # The API router is defined in the `core` app (core/urls.py)
     path('api/', include('core.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
